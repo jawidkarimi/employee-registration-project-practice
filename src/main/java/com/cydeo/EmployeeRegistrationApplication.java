17eb12dc13afb0ace0ee1ddcfe269c078c126b81
jawidkarimi <104651569+jawidkarimi@users.noreply.github.com>
2022-10-06 22:29:15 -0700

    create employee controller

--- src/main/java/com/cydeo/EmployeeRegistrationApplication.java
+++ src/main/java/com/cydeo/EmployeeRegistrationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeRegistrationProjectPracticeApplication {
+public class EmployeeRegistrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeRegistrationProjectPracticeApplication.class, args);
+        SpringApplication.run(EmployeeRegistrationApplication.class, args);
     }
 
 }
